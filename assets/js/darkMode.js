902fe50ec1a44dc8a473d6ca5aff84218ecfaa35
Paloma Rod <placeholder.prod@outlook.com>
2025-01-14 15:00:09 -0600

    dark mode

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,24 +1,20 @@
 const toggle = document.getElementById("toggle");
-const toggleText = document.getElementById("toggle-text");
+const navBar = document.querySelector(".nav-bars");  // Use querySelector for a single element
 
 toggle.addEventListener('click', () => {
-    const isDarkMode = toggle.classList.contains('dark');
+    const isDarkMode = !toggle.classList.contains('light');
 
     if (isDarkMode) {
-        toggle.classList.remove('dark');
-        toggleText.textContent = 'Light Mode';
+        toggle.classList.add('light');
 
-        // Change <pre> background for light mode
-        document.querySelectorAll('pre').forEach(pre => {
-            pre.style.backgroundColor = '#e4e4e4';
-        });
+        document.body.style.backgroundColor = '#e4e4e4';
+        document.body.style.color = '#333';
+        navBar.style.filter = 'invert(22%) sepia(0%) saturate(33%) hue-rotate(210deg) brightness(86%) contrast(96%)';
     } else {
-        toggle.classList.add('dark');
-        toggleText.textContent = 'Dark Mode';
+        toggle.classList.remove('light');
 
-        // Change <pre> background for dark mode
-        document.querySelectorAll('pre').forEach(pre => {
-            pre.style.backgroundColor = '#333';
-        });
+        document.body.style.backgroundColor = '#333';
+        document.body.style.color = '#fff';
+        navBar.style.filter = 'invert(100%) sepia(0%) saturate(0%) hue-rotate(258deg) brightness(107%) contrast(105%)';
     }
 });
